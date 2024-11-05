f61c444d0377f12b3c9f46f9d2c64e11c4beb41a
IDLY <oleg433242new@gmail.com>
2024-11-05 23:35:56 +0300

    Update PRAKRABno2.py

--- Zadachi-OAP/29.10.24 --/PRAKRABno2.py
+++ Zadachi-OAP/29.10.24 --/PRAKRABno2.py
@@ -1,9 +1,23 @@
 import math
 
-x = float(input('Введите значение X: '))
-y = float(input('Введите значение Y: '))
+try:
+    x = float(input('Введите значение X: '))
+    y = float(input('Введите значение Y: '))
+    
+    under_sqrt = x**7 - y**3
+    
+    if under_sqrt < 0:
+        print("Ошибка: попытка вычислить квадратный корень из отрицательного числа.")
+    else:
 
-f = (x**5+(math.pow(math.cos(x**2),3))/(3.14*3.57*math.sin(y)+math.sqrt(x**7 - y**3)))
+        denominator = 3.14 * 3.57 * math.sin(y) + math.sqrt(under_sqrt)
+        
+        if denominator == 0:
+            print("Ошибка: деление на ноль невозможно.")
+        else:
+           
+            f = (x**5 + (math.cos(x**2)**3)) / denominator
+            print('Значение f равно:', f)
 
-print('Значение f равно:' , f)
-
+except ValueError:
+    print("Ошибка: введено некорректное значение. Пожалуйста, введите числовые значения.")
