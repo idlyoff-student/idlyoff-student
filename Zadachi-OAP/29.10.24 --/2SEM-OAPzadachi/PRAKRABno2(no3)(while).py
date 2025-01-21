4f8c65efed71efaa64a30818f9365290f54ae86d
IDLY <oleg433242new@gmail.com>
2025-01-21 11:59:15 +0300

    Update PRAKRABno2(no3)(while).py

--- Zadachi-OAP/29.10.24 --/2SEM-OAPzadachi/PRAKRABno2(no3)(while).py
+++ Zadachi-OAP/29.10.24 --/2SEM-OAPzadachi/PRAKRABno2(no3)(while).py
@@ -1,1 +1,11 @@
+A = int(input("Введите A (A > B): "))
+B = int(input("Введите B: "))
 
+if A <= B:
+    print("Ошибка: A должно быть больше B.")
+else:
+    occupied = 0
+    while occupied + B <= A:
+        occupied += B        
+        free = A - occupied
+        print("Длина незанятой части A:", free)
