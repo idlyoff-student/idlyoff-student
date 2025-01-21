bb80f036d28184409a551612793c73912003416c
IDLY <oleg433242new@gmail.com>
2025-01-21 12:24:04 +0300

    Update PRAKRABno1(while).py

--- Zadachi-OAP/29.10.24 --/2SEM-OAPzadachi/PRAKRABno1(while).py
+++ Zadachi-OAP/29.10.24 --/2SEM-OAPzadachi/PRAKRABno1(while).py
@@ -1,9 +1,9 @@
 total_sum = 0
-number = 0  # Инициализируем переменную number
+number = 0
 
 while number != 100:
     number = int(input("Введите число (100 для завершения): "))
-    if number != 100:  # Проверяем, не равно ли число 100
+    if number != 100: 
         total_sum += number
 
 print("Сумма введенных чисел:", total_sum)
