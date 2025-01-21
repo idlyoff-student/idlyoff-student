805ebd8e3a7195ffa58ddb9869768d0b65fa7672
IDLY <oleg433242new@gmail.com>
2025-01-21 12:05:39 +0300

    Update PRAKRABno2(no4)(+)(while).py

--- Zadachi-OAP/29.10.24 --/2SEM-OAPzadachi/PRAKRABno2(no4)(+)(while).py
+++ Zadachi-OAP/29.10.24 --/2SEM-OAPzadachi/PRAKRABno2(no4)(+)(while).py
@@ -1,1 +1,19 @@
+even_count = 0
+number = None
+
+while number != 0:
+    number = int(input("Введите число (0 для завершения): "))
+    
+    if number == 0:
+        break
+    
+    if number > 0:
+        number = number / 2
+    elif number < 0:
+        number = number * 2
+    
+    if number % 2 == 0:
+        even_count += 1
+
+print("Количество четных чисел:", even_count)
 
