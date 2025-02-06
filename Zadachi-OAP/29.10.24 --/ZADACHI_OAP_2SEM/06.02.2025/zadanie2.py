57a46e43866e1ec436dca86b9d6ecf2a5ecd9715
IDLY <oleg433242new@gmail.com>
2025-02-06 11:52:22 +0300

    Update zadanie2.py

--- Zadachi-OAP/29.10.24 --/ZADACHI_OAP_2SEM/06.02.2025/zadanie2.py
+++ Zadachi-OAP/29.10.24 --/ZADACHI_OAP_2SEM/06.02.2025/zadanie2.py
@@ -1,1 +1,21 @@
+# ВАРИАНТ 7
+# src code
 
+# Вводим массив целых чисел
+input_array = input("Введите целые числа через пробел: ")
+numbers = list(map(int, input_array.split()))
+
+# Инициализируем сумму и произведение (еще один метод?)
+sum_even_indices = 0
+product_odd_indices = 1
+
+# Проходим по элементам массива
+for index in range(len(numbers)):
+    if index % 2 == 0:  # Четные индексы
+        sum_even_indices += numbers[index]
+    else:  # Нечетные индексы
+        product_odd_indices *= numbers[index]
+
+# Выводим результат
+print("Сумма элементов с четными номерами:", sum_even_indices)
+print("Произведение элементов с нечетными номерами:", product_odd_indices)
