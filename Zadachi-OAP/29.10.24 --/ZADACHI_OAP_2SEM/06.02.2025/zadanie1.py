7ffe2ec6495f99cd0e9dd113da30217b64c61759
IDLY <oleg433242new@gmail.com>
2025-02-06 11:44:50 +0300

    Update zadanie1.py

--- Zadachi-OAP/29.10.24 --/ZADACHI_OAP_2SEM/06.02.2025/zadanie1.py
+++ Zadachi-OAP/29.10.24 --/ZADACHI_OAP_2SEM/06.02.2025/zadanie1.py
@@ -1,1 +1,18 @@
+# ВАРИАНТ 7
+# SRC code
 
+input_string = input("Введите строку: ")
+n = len(input_string)
+half_n = n // 2
+char_list = list(input_string)
+i = 0
+
+while True:
+    if i >= half_n:
+        break
+    if char_list[i] == 'п':
+        char_list[i] = '*'
+    i += 1
+
+result_string = ''.join(char_list)
+print("Результат:", result_string)
