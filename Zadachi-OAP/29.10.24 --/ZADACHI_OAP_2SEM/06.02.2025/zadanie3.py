4faedff346778a2f827bd8d5d4faedd6a12b5faa
IDLY <oleg433242new@gmail.com>
2025-02-10 12:04:56 +0300

    Update zadanie3.py

--- Zadachi-OAP/29.10.24 --/ZADACHI_OAP_2SEM/06.02.2025/zadanie3.py
+++ Zadachi-OAP/29.10.24 --/ZADACHI_OAP_2SEM/06.02.2025/zadanie3.py
@@ -1,1 +1,13 @@
+# ВАРИАНТ 7
+# SCR CODE: 2. Переставить в одномерном массиве минимальный элемент и максимальный.
 
+arr = list(map(int, input("Введите числа через пробел: ").split()))
+
+min_value = min(arr)
+max_value = max(arr)
+
+min_index = arr.index(min_value)
+max_index = arr.index(max_value)
+arr[min_index], arr[max_index] = arr[max_index], arr[min_index]
+
+print(arr)
