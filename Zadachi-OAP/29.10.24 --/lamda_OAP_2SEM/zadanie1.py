2dced18244e21beb0ec9b39eb98bdbf499a8e75a
IDLY <oleg433242new@gmail.com>
2025-04-08 12:07:24 +0300

    Update zadanie1.py

--- Zadachi-OAP/29.10.24 --/lamda_OAP_2SEM/zadanie1.py
+++ Zadachi-OAP/29.10.24 --/lamda_OAP_2SEM/zadanie1.py
@@ -13,3 +13,16 @@ x = float(input('Введите значение X: '))
 
 result = calculate_y(a, b, x)
 print(f"Значение y: {result}")
+
+#РЕШЕНИЕ ЧЕРЕЗ LAMBDA
+
+#import math
+
+#calculate_y = lambda a, b, x: (math.cos(b) - math.sqrt(a)) / (3 * a - abs(x))
+
+#a = 4  
+#b = math.pi / 3 
+#x = 2 
+
+#result = calculate_y(a, b, x)
+#print(f"Значение y: {result}")
