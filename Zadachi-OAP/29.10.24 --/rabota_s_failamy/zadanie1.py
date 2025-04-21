e5e14a87a56baf394b9b1a0492eefdb42ec5eac0
IDLY <oleg433242new@gmail.com>
2025-04-21 11:54:29 +0300

    Update zadanie1.py

--- Zadachi-OAP/29.10.24 --/rabota_s_failamy/zadanie1.py
+++ Zadachi-OAP/29.10.24 --/rabota_s_failamy/zadanie1.py
@@ -8,25 +8,37 @@ def f2(x):
     y = pow(2, log(3 - cos(pi / 4 + 2 * x), 3 + sin(x)) / (1 + tan(2 * x / pi)**2))
     return y
 
-fi = open("lab1_pb_in.txt", "rt")
-fo = open("lab1_pb_ou.txt", "wt")
+try:
+    fi = open("lab1_pb_in.txt", "rt")
+except FileNotFoundError:
+    print("Входной файл 'lab1_pb_in.txt' не найден. Создаю новый файл с примерными данными.")
+    with open("lab1_pb_in.txt", "wt") as fi:
+        fi.write("1.0 0.5\n")
+        fi.write("2.0 1.0\n")
+        fi.write("3.0 1.5\n")
+    fi = open("lab1_pb_in.txt", "rt")
 
-line = fi.readline()  # Пропустить заголовок в файле
-# Вывести шапку таблицы в файл
+try:
+    fo = open("lab1_pb_ou.txt", "wt")
+except IOError:
+    print("Ошибка: Не удалось открыть выходной файл 'lab1_pb_ou.txt'.")
+    fi.close()
+    exit(1)
+
+line = fi.readline()
 fo.write("+======+======+=========+========+\n")
 fo.write("I A I X I F1 I F2 I\n")
 fo.write("+======+======+=========+========+\n")
 
-for line in fi:  # Для всех строк файла
+for line in fi:
     if line == "\n":
         continue
-    (b, c) = line.split()  # Расщепить
-    a = float(b)  # Привести к вещественному типу
+    (b, c) = line.split()
+    a = float(b)
     x = float(c)
-    # Вывод в файл
     fo.write("I {0: .1f} I {1: .1f} I {2: 5.4f} I".format(a, x, f1(a, x)))
     fo.write("{0: 6.4f} I\n".format(f2(x)))
     fo.write("+------+------+---------+--------+\n")
 
-fi.close()  # Закрываем файлы
+fi.close()
 fo.close()
